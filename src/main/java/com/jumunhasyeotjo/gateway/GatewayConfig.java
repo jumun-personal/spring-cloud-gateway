fbd1263f1e96de07b7b3418c89ad075fbe4bffda
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2026-01-03 14:09:39 +0900

    fix: 내부 alb -> 로컬 환경 라우팅 전환

--- src/main/java/com/jumunhasyeotjo/gateway/GatewayConfig.java
+++ src/main/java/com/jumunhasyeotjo/gateway/GatewayConfig.java
@@ -1,6 +1,5 @@
 package com.jumunhasyeotjo.gateway;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,21 +7,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayConfig {
-    @Value("${alb.url}")
-    private String albUrl;
-    @Value("${alb.user-port}")
-    private Integer userPort;
-    @Value("${alb.order-port}")
-    private Integer orderPort;
-    @Value("${alb.hub-port}")
-    private Integer hubPort;
 
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("user-service", r -> r
                         .path("/api/v1/auth/**", "/api/v1/users/**", "/api/v1/messages/**", "/api/v1/passports/**")
-                        .uri(albUrl + ":" + userPort)
+                        .uri("lb://user-service")
                 )
                 .route("user-service-actuator", r -> r
                         .path("/monitoring/user-service/actuator/**")
@@ -30,15 +21,15 @@ public class GatewayConfig {
                                 "/monitoring/user-service/(?<segment>.*)",
                                 "/${segment}"
                         ))
-                        .uri(albUrl + ":" + userPort)
+                        .uri("lb://user-service")
                 )
                 .route("order-to-shipping-service", r -> r
                         .path("/api/v1/orders/**", "/api/v1/coupons/**", "/api/v1/shippings/**", "/api/v1/payments/**", "/api/v1/shipping-histories/**")
-                        .uri(albUrl + ":" + orderPort)
+                        .uri("lb://order-to-shipping-service")
                 )
                 .route("hub-product-stock-company", r -> r
                         .path("/api/v1/hubs/**", "/api/v1/stocks/**", "/api/v1/products/**", "/api/v1/companies/**")
-                        .uri(albUrl + ":" + hubPort)
+                        .uri("lb://hub-product-stock-company")
                 )
                 .build();
     }
